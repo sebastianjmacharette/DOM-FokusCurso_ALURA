081a607b5b9333e8122fbac1ea25cd860f6b6946
sebastianjmacharette <dev@macharette.com.ar>
2024-10-19 05:58:13 -0300

    update

--- script.js
+++ script.js
@@ -4,22 +4,31 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
-
+const botones = document.querySelectorAll('.app__card-button')
 
 botonCorto.addEventListener('click', () => {
   cambiarContexto('descanso-corto');
+  botonCorto.classList.add('active')
 
 });
 
 botonEnfoque.addEventListener('click', () => {
  cambiarContexto('enfoque');
+ botonEnfoque.classList.add('active')
+
 });
 
 botonLargo.addEventListener('click', ()=>{
    cambiarContexto('descanso-largo');
+   botonLargo.classList.add('active')
 });
 
 function cambiarContexto (contexto){
+
+  botones.forEach(function(contexto){
+    contexto.classList.remove('active');
+  })
+
   html.setAttribute('data-contexto', contexto);
   banner.setAttribute('src', `./imagenes/${contexto}.png`); // Cerrar el paréntesis aquí
   switch (contexto) {
